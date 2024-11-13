9c289da1650485fc72f438cd55dc1e4c5d1f4d4e
codelangs <166025318+codelangsaurangabad@users.noreply.github.com>
2024-11-13 14:13:17 +0530

    first project upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const adminRoutes = require('./routes/admin');
+const superAdminRoutes = require('./routes/superAdmin');
+const salespersonRoutes = require('./routes/salesperson');
+const customerRoutes = require('./routes/customer');
+const authRoutes = require('./routes/auth');
+const connectDB = require('./config/db');
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Routes
+app.use('/super-admin', superAdminRoutes);
+app.use('/admin', adminRoutes);
+app.use('/salesperson', salespersonRoutes);
+app.use('/customer', customerRoutes);
+app.use('/auth', authRoutes);
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
